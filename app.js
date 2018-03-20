8db50bb7b8e9c817e248ea623cf379f460838148
circ0 <deivy.g1@gmail.com>
2018-03-19 22:19:18 -0700

    Usuario y token listo

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(bodyParser.json());
 // Importar Rutas
 var appRoutes = require('./routes/app');
 var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 
 // Conexion a la base de datos
@@ -29,7 +30,9 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
 
 // Rutas
 app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
 app.use('/', appRoutes);
+
 
 
 
